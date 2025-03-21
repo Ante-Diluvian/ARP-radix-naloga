cfe792e50743e537642288a919730b07b6f7615a
Val <val.smodis@student.um.si>
2025-03-21 14:35:18 +0100

    Narejena funkcija za branje števil iz vhodne datoteke

---
+++ dn1.cpp
@@ -0,0 +1,27 @@
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <climits>
+using namespace std;
+
+bool Branje_Stevil(vector<int> &vec, const char s[]) {
+	ifstream input(s);
+	int st;
+
+	if (!input.is_open()) {
+		return false;
+	}
+
+	while (!input.eof()) {
+		input >> st;
+		vec.push_back(st);
+		while (isspace(input.peek())) input.get();
+	}
+	input.close();
+	return true;
+}
+
+int main(int argc, const char* argv[]) {
+
+	return 0;
+}
