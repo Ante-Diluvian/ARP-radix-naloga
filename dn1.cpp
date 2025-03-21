20be33791a774fd3d4fe834256b103c5b0739d0b
Val <val.smodis@student.um.si>
2025-03-21 15:37:18 +0100

    Narejena funkcija za dobivanje K-tega bita

--- dn1.cpp
+++ dn1.cpp
@@ -28,10 +28,23 @@ void Izpis_Stevil(int* polje, unsigned int velikost) {
 		output << polje[i] << ' ';
 }
 
+
+vector<int> extractKthBit(const vector<int>& A, int k) {
+    vector<int> D;
+    for (int num : A) {
+        int bit = (num >> k) & 1;
+        D.push_back(bit);
+    }
+    return D;
+}
+
 int main(int argc, const char* argv[]) {
 	vector<int> A;
+	int k = 0;
 
 	if (!Branje_Stevil(A, argv[1])) return 0;
+
+	vector<int> D = extractKthBit(A, k);
 
 	Izpis_Stevil(&A[0], A.size());
 
